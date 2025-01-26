dbe074ab22415ae25326ca3c87e023089b749b3b
caoshiyi <shicao@berkeley.edu>
2025-01-26 09:43:05 -0800

    .

--- data_gen1.sh
+++ data_gen1.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
+conda activate sky-t1
+cd /SkyThought/skythought/tools
 # Define the datasets
 datasets=("math" "gsm8k" "amc_aime")
 result_dir="./data"
 mkdir -p $result_dir
 # Define the command template
 command_template="python inference_and_check.py --dataset NUMINA --model deepseek-ai/DeepSeek-R1-Distill-Qwen-32B --tp 8 --max_tokens 16384 --split train --source {dataset} --filter-difficulty --result-dir ./data --inference --math-difficulty-lower-bound 0 --math-difficulty-upper-bound 7"
-
-# Create a base directory for logs
-log_dir="./logs"
-mkdir -p $log_dir
 
 # Loop through each dataset and execute the command
 for dataset in "${datasets[@]}"; do
@@ -21,5 +18,4 @@ for dataset in "${datasets[@]}"; do
     echo "Command: $command"
     # Execute the command and redirect output to the log file
     $command
-    echo "Finished processing dataset: $dataset. Logs saved to $log_file"
 done
